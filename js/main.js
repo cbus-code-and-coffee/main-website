fdc9b2a87bd2aaca13bcc196fe28fb3c649bf065
Nathan Speich <nathanspeich@icloud.com>
2022-10-03 19:37:50 -0400

    Added responsiveness to main nav

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Variables
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+const navItem = document.querySelector(".navItem");
+
+//Event Listeners / Functions
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+navItem.forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
